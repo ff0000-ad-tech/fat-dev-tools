fde549ca78724eefde38514024757372fa9e47ec
Derek Miranda <derek.miranda@ff0000.com>
2018-03-20 17:08:28 -0700

    Added Git link sniffer bin file and deps

---
+++ bin/sniff-git-links.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const pickBy = require('lodash/pickBy')
+const pick = require('lodash/pick')
+const colors = require('colors/safe')
+
+function isGitLink(version) {
+	return /\.git(#[\w-]*)?$/.test(version)
+}
+
+function isFAT(depName) {
+	return /^\@ff0000-ad-tech/.test(depName)
+}
+
+function findGitLinksInDeps(deps) {
+	return pickBy(deps, isGitLink)
+}
+
+function onlyFATDeps(deps) {
+	const fatDepNames = Object.keys(deps).filter(isFAT)
+	return pick(deps, fatDepNames)
+}
+
+function findGitLinksInDirectories(dirs) {
+	return dirs
+		.reduce((accum, dir) => {
+			const packageJson = path.resolve(dir, 'package.json')
+			try {
+				const pkgJsonContents = fs.readFileSync(packageJson, 'utf8')
+				const parsedObj = JSON.parse(pkgJsonContents)
+				const deps = Object.assign({},
+					parsedObj.dependencies || {},
+					parsedObj.devDependencies || {},
+					parsedObj.peerDependencies || {}
+				)
+				const fatDeps = onlyFATDeps(deps)
+				const gitLinkDeps = findGitLinksInDeps(fatDeps)
+				Object.keys(gitLinkDeps).length && (accum[dir] = gitLinkDeps)
+				return accum
+			} catch (err) {
+				if (err.code !== 'ENOENT') throw err
+				return accum
+			}
+		}, {})
+}
+
+function findFatGitLinks() {
+	const files = fs.readdirSync(process.cwd())
+	const dirPaths = files
+		.map(file => path.resolve(process.cwd(), file))
+		.filter(filepath => {
+			const stats = fs.statSync(filepath)
+			return stats.isDirectory()
+		})
+	const gitLinkDepsByDir = findGitLinksInDirectories(dirPaths)
+
+	Object.keys(gitLinkDepsByDir).forEach(dirPath => {
+		console.log()
+		console.log(colors.blue.underline(dirPath))
+		const gitLinkDeps = gitLinkDepsByDir[dirPath]
+		Object.keys(gitLinkDeps).forEach(depName => {
+			const gitLink = gitLinkDeps[depName]
+			const styledDepName = colors.green(depName)
+			const styledGitLink = colors.yellow(gitLink)
+			console.log(`${styledDepName} => ${styledGitLink}`)
+		})
+	})
+}
+
+findFatGitLinks()
